676d718810b9f2211c55f07834cbf8c201d3ad96
dlyr <github@dlyr.fr>
2020-07-05 08:25:49 +0200

    add ref push

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,9 @@
 cd "$GITHUB_WORKSPACE"
 
 /run-clang-format.py $*
-
+ehco "$GITHUB_REF"
 git log --pretty=oneline -1
 git config user.email "clang-formater@radium-engine.git"
 git config user.name "clang-formatter"
 git commit -a -m"*** clang-format only ***"
-git push
-
+git push "$GITHUB_REF"
