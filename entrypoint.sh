1e1aae312fe8d790ccfcbfe94a5f28b8d491ac50
dlyr <github@dlyr.fr>
2020-07-05 07:42:30 +0200

    name

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ cd "$GITHUB_WORKSPACE"
 #/run-clang-format.py $*
 
 touch "toto"
-git config --global user.email "clang-formater@radium-engine.git"
-git config --global user.name "clang-formatter"
+git config user.email "clang-formater@radium-engine.git"
+git config  user.name "clang-formatter"
 git add toto
 git commit -m"toto"
 git push
