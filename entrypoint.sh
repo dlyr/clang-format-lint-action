9d0dd7a413851479c0d4c1a84666b22b76332e2f
dlyr <github@dlyr.fr>
2020-07-05 11:17:22 +0200

    do not commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,31 +5,3 @@ cd "$GITHUB_WORKSPACE"
 /run-clang-format.py $*
 
 exit 0
-
-echo "$GITHUB_REF"
-git log --pretty=oneline -1
-git branch -a -vvv
-
-echo "### Getting branch"
-BRANCH=${GITHUB_REF#*refs/heads/}
-echo "### git fetch $BRANCH ..."
-git fetch origin $BRANCH
-echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
-git checkout $BRANCH
-
-echo "## Configuring git author..."
-git config user.email "clang-formater@radium-engine.git"
-git config user.name "clang-formatter"
-
-# Ignore workflow files (we may not touch them)
-git update-index --assume-unchanged .github/workflows/*
-
-echo "## Running clang-format on C/C++ source"
-
-/run-clang-format.py $*
-
-echo "## Commiting files..."
-git commit -a -m"*** clang-format only ***" || true
-
-echo "## Pushing to $BRANCH"
-git push -u origin $BRANCH
