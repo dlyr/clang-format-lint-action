22c591f7509155f8b217fc3387bb46be3c5cd0f2
dlyr <github@dlyr.fr>
2020-07-05 08:36:31 +0200

    trying to get push work

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,35 @@
 
 cd "$GITHUB_WORKSPACE"
 
-/run-clang-format.py $*
+
 echo "$GITHUB_REF"
 git log --pretty=oneline -1
+
+
+
+#echo "### Adding git remote..."
+#git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+
+echo "### Getting branch"
+BRANCH=${GITHUB_REF#*refs/heads/}
+echo "### git fetch $BRANCH ..."
+git fetch origin $BRANCH
+echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
+git checkout $BRANCH
+
+echo "## Configuring git author..."
 git config user.email "clang-formater@radium-engine.git"
 git config user.name "clang-formatter"
-git commit -a -m"*** clang-format only ***"
-git push "$GITHUB_REF"
+
+# Ignore workflow files (we may not touch them)
+git update-index --assume-unchanged .github/workflows/*
+
+echo "## Running clang-format on C/C++ source"
+
+/run-clang-format.py $*
+
+echo "## Commiting files..."
+git commit -a -m"*** clang-format only ***" || true
+
+echo "## Pushing to $BRANCH"
+git push -u origin $BRANCH
